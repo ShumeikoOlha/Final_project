c019caee8e692300faf8835a81de3f877ad0641a
Shumeiko Olha <shmeyko.olya@gmail.com>
2017-12-08 11:50:59 +0000

    add

--- app/main.js
+++ app/main.js
@@ -2,3 +2,23 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('spinner').classList.remove('spinner');
     document.getElementById('preloader').classList.remove('preloader');
 });
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    $('.scrollToTop').click(function(event){
+        $('html, body').animate({scrollTop : 0},800);
+        event.preventDefault();
+    });
+    $('.nav_item__link').on('click', function (event) {
+        var anchor = $(this);
+        $('html, body').animate({
+            scrollTop:($(anchor.attr('href')).offset().top - 50)
+        }, 1250);
+        event.preventDefault();
+    });
+});
